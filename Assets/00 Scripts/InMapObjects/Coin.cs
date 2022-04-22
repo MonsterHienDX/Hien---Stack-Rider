cb3655da067a5d41986658863639d970583289d6
MonsterHienDX <hiendx@monsterstudio.io>
2022-04-22 11:44:45 +0700

    add coin floating when reached

--- Assets/00 Scripts/InMapObjects/Coin.cs
+++ Assets/00 Scripts/InMapObjects/Coin.cs
@@ -6,8 +6,48 @@ public class Coin : MonoBehaviour
 {
     public int coinAmount;
     public float rotateSpeed;
+    public float lastShown;
+    public float floatingDuration;
+    private float remainingDuration = 1f;
+    public bool isCollected = false;
+    [SerializeField] private MeshRenderer meshRenderer;
+    private BoxCollider boxCollider;
+    private void Awake()
+    {
+        boxCollider = GetComponent<BoxCollider>();
+    }
     void Update()
     {
         transform.Rotate(0, rotateSpeed, 0, Space.World);
+        if (isCollected)
+        {
+            UpdateFloatingCoin();
+        }
+    }
+
+    public void ChangeColorAlpha(float alpha)
+    {
+        Color currentColor = meshRenderer.material.GetColor("_BaseColor");
+        Color newColor = new Color(currentColor.r, currentColor.g, currentColor.b, alpha);
+        meshRenderer.material.SetColor("_BaseColor", newColor);
+    }
+
+    public void CollideWithPlayer()
+    {
+        isCollected = true;
+        lastShown = Time.time;
+        boxCollider.enabled = false;
+    }
+
+    public void UpdateFloatingCoin()
+    {
+        if (Time.time - lastShown > floatingDuration)
+        {
+            Destroy(this.gameObject);
+        }
+
+        transform.position += Vector3.up * 3 * Time.deltaTime;
+        remainingDuration -= Time.deltaTime;
+        ChangeColorAlpha((floatingDuration * remainingDuration) * 255);
     }
 }
